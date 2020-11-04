be03d08fe9b22889211c9e6e20c5abb05857f58b
Jitendrap1702 <56450901+Jitendrap1702@users.noreply.github.com>
2020-11-04 16:25:40 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/recyclercart/MainActivity.java
+++ app/src/main/java/com/example/recyclercart/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclercart;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private ActivityMainBinding b;
     @Override
@@ -19,8 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-
         setupProductList();
+
+        WeightPicker weightPicker = new WeightPicker();
+        weightPicker.show(this, new WeightPicker.OnWeightPickedListener() {
+            @Override
+            public void onWeightPicked(int kg, int g) {
+
+                Toast.makeText(MainActivity.this, "The item quantity is "+kg+" in kgs",Toast.LENGTH_SHORT).show();
+                Log.d(LOG_TAG, "quantity is "+kg+" kgs");
+                Toast.makeText(MainActivity.this, "The item quantity is "+g+" in gms",Toast.LENGTH_SHORT).show();
+                Log.d(LOG_TAG, "quantity is "+g+" gms");
+            }
+
+            @Override
+            public void onWeightPickerCancelled() {
+
+                Toast.makeText(MainActivity.this, "Order Cancelled",Toast.LENGTH_SHORT);
+                Log.d(LOG_TAG, "order cancelled");
+            }
+        });
     }
 
     private void setupProductList() {
